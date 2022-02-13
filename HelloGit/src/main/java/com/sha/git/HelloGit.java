6c78e0468a7ca4995c05a6d5078cb78c737ade65
Sha888 <295028635@qq.com>
2022-02-13 11:14:38 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/sha/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.sha.git;
+
+/**
+ * @author Sha
+ * @create 2022-02-13 11:04
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
